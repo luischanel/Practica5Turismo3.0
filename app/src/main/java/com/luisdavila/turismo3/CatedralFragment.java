8402ac76038a606ffb35f2ddcb2a4bfcf9dd1670
Luis <luischanel16@gmail.com>
2016-05-02 22:52:08 -0500

    Initial commit

---
+++ app/src/main/java/com/luisdavila/turismo3/CatedralFragment.java
@@ -0,0 +1,32 @@
+package com.luisdavila.turismo3;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CatedralFragment extends Fragment {
+
+
+
+    public CatedralFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_catedral, container, false);
+    }
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/luisdavila/turismo3/MiradorFragment.java
@@ -0,0 +1,29 @@
+package com.luisdavila.turismo3;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MiradorFragment extends Fragment {
+
+
+    public MiradorFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mirador, container, false);
+    }
+
+}
